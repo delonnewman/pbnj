1ffbe90e53d8f6a34f483feb47e82d5bac5d5459
Delon Newman <contact@delonnewman.name>
2025-12-05 17:57:20 -0500

    Add path->file

--- src/clj/pbnj/paths.clj
+++ src/clj/pbnj/paths.clj
@@ -1,6 +1,20 @@
-(ns pbnj.paths)
+(ns pbnj.paths
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (deftype Path [name])
 
 (defn path-reader [string]
   (Path. string))
+
+(defn path->file
+  ([path] (io/as-file (.name path)))
+  ([parent path] (io/file parent (.name path)))
+  ([parent path ext]
+   (let [ext (if (str/starts-with? ext ".") (.substring ext 1) ext)]
+     (io/file parent (str (.name path) "." ext)))))
+
+(comment
+  (path->file "resources" #pbnj/path "welcome/index" "html")
+  (path->file "resources" #pbnj/path "welcome/index" ".html")
+  )
