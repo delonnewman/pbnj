bfe3143cf0bb5dd5658a71241c6b6afb0a127a7b
Delon R. Newman <contact@delonnewman.name>
2025-12-05 19:00:20 -0500

    Add path-exists?

--- src/clj/pbnj/paths.clj
+++ src/clj/pbnj/paths.clj
@@ -14,7 +14,13 @@
    (let [ext (if (str/starts-with? ext ".") (.substring ext 1) ext)]
      (io/file parent (str (.name path) "." ext)))))
 
+(defn path-exists?
+  ([path] (.exists (path->file path)))
+  ([parent path] (.exists (path->file parent path)))
+  ([parent path ext] (.exists (path->file parent path ext))))
+
 (comment
   (path->file "resources" #pbnj/path "welcome/index" "html")
   (path->file "resources" #pbnj/path "welcome/index" ".html")
+  (path-exists? "src/clj" #pbnj/path "pbnj/paths" "clj")
   )
