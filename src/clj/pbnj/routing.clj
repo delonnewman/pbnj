4f7aad45dfbc240b2461b0cef1e1a96710ab9cab
Delon R. Newman <contact@delonnewman.name>
2025-12-05 19:29:37 -0500

    Add page and route?

--- src/clj/pbnj/routing.clj
+++ src/clj/pbnj/routing.clj
@@ -1,1 +1,24 @@
 (ns pbnj.routing)
+
+(defn page
+  [path
+   & {:keys [name to via formats]
+      :or {via :get formats #{:html}}}]
+  {:route/path path
+   :route/src to
+   :route/name name
+   :route/method via
+   :route/formats formats})
+
+(defn route? [r]
+  (and
+   (map? r)
+   (contains? r :route/path)
+   (contains? r :route/src)
+   (contains? r :route/method)))
+
+(comment
+  (page "/" :name "root" :to #pbnj/path "welcome/index")
+  (let [r (page "/" :name "root" :to #pbnj/path "welcome/index")]
+    (route? r))
+  )
