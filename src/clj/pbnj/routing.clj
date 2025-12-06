8ae5499ef3fd09f5a38a587baad678e37533390e
Delon R. Newman <contact@delonnewman.name>
2025-12-05 19:40:14 -0500

    Add routes

--- src/clj/pbnj/routing.clj
+++ src/clj/pbnj/routing.clj
@@ -17,8 +17,19 @@
    (contains? r :route/src)
    (contains? r :route/method)))
 
+(defn routes
+  ([r] (if (route? r) #{r} r))
+  ([r & rs] (set (concat (routes r) rs))))
+
 (comment
   (page "/" :name "root" :to #pbnj/path "welcome/index")
+
   (let [r (page "/" :name "root" :to #pbnj/path "welcome/index")]
     (route? r))
+
+  (routes
+   (page "/" :name "root" :to #pbnj/path "welcome/index")
+   (page "/entities" :name "entities" :to #pbnj/path "entities/list")
+   (page "/entities/:id" :name "entities" :to #pbnj/path "entities/show")
+   (page "/entities/:id" :name "entities" :to #pbnj/path "entities/update" :via :post))
   )
