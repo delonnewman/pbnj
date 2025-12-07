387a12719576ecca1af783152b06e7c509a5bf5c
Delon R. Newman <contact@delonnewman.name>
2025-12-06 20:17:45 -0500

    Add path and fetch

--- src/clj/pbnj/paths.clj
+++ src/clj/pbnj/paths.clj
@@ -12,17 +12,42 @@
 
 (defn root? [p] (= (.name p) "/"))
 
+(defn- with-ext
+  [file ext]
+  (let [ext (if (str/starts-with? ext ".") (.substring ext 1) ext)]
+    (str file "." ext)))
+
+(comment
+  (with-ext "welcome" "html")
+  (with-ext "welcome" ".html")
+  )
+
 (defn path->file
   ([path] (io/as-file (.name path)))
   ([parent path] (io/file parent (.name path)))
   ([parent path ext]
-   (let [ext (if (str/starts-with? ext ".") (.substring ext 1) ext)]
-     (io/file parent (str (.name path) "." ext)))))
+   (io/file parent (with-ext (.name path) ext))))
 
 (defn exists?
   ([path] (.exists (path->file path)))
   ([parent path] (.exists (path->file parent path)))
   ([parent path ext] (.exists (path->file parent path ext))))
+
+(defn path
+  [& parts]
+  (->> parts
+       (map #(if (path? %) (.name %) (str %)))
+       (str/join "/")
+       ->Path))
+
+(defn fetch
+  ([path] (when (exists? path) path))
+  ([parent path']
+   (when (exists? parent path')
+     (path parent path')))
+  ([parent path' ext]
+   (when (exists? parent path' ext)
+     (path parent (with-ext (.name path') ext)))))
 
 (defn parts [path]
   (if (root? path)
@@ -37,3 +62,11 @@
       (path->file parent path (name format)))
     parents formats)
    (filter java.io.File/.exists)))
+
+(comment
+  (fetch #pbnj/path "test/resources/welcome/index.html")
+  (fetch #pbnj/path "test/resources/welcome/index.php")
+  (.name (fetch "test/resources" #pbnj/path "welcome/index" "html"))
+
+  (path "test/resources" #pbnj/path "welcome" "index.html")
+  )
