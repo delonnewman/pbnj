457068e2556be057efc28d92deb6f74cf526fffd
Delon R. Newman <contact@delonnewman.name>
2025-12-06 19:43:52 -0500

    Add exists? tests

--- test/pbnj/paths_test.clj
+++ test/pbnj/paths_test.clj
@@ -12,14 +12,24 @@
     (is (= expected actual))))
 
 (deftest path->file-test
-  (let [expected (io/file "resources/welcome/index.html")]
+  (let [expected (io/file "test/resources/welcome/index.html")]
     (testing "just path"
-      (is (= expected (path/path->file #pbnj/path "resources/welcome/index.html"))))
+      (is (= expected (path/path->file #pbnj/path "test/resources/welcome/index.html"))))
     (testing "parent and path"
-      (is (= expected (path/path->file "resources" #pbnj/path "welcome/index.html"))))
+      (is (= expected (path/path->file "test/resources" #pbnj/path "welcome/index.html"))))
     (testing "parent, path and extension"
-      (let [file (path/path->file "resources" #pbnj/path "welcome/index" "html")]
+      (let [file (path/path->file "test/resources" #pbnj/path "welcome/index" "html")]
         (is (= expected file))))
     (testing "parent, path and extension with a leading dot"
-      (let [file (path/path->file "resources" #pbnj/path "welcome/index" ".html")]
+      (let [file (path/path->file "test/resources" #pbnj/path "welcome/index" ".html")]
         (is (= expected file))))))
+
+(deftest exists?-test
+  (testing "path exists"
+    (is (path/exists? #pbnj/path "test/resources/welcome/index.html"))
+    (is (path/exists? "test/resources" #pbnj/path "welcome/index.html"))
+    (is (path/exists? "test/resources" #pbnj/path "welcome/index" "html")))
+  (testing "path doesn't exist"
+    (is (not (path/exists? #pbnj/path "test/resources/welcome/index.php")))
+    (is (not (path/exists? "test/resources" #pbnj/path "welcome/index.php")))
+    (is (not (path/exists? "test/resources" #pbnj/path "welcome/index" "php")))))
