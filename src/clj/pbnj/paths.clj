50e88735f3a5aaab0016426c36cbc6aa5cdb58c4
Delon R. Newman <contact@delonnewman.name>
2025-12-08 20:12:50 -0500

    Make paths more abstract

--- src/clj/pbnj/paths.clj
+++ src/clj/pbnj/paths.clj
@@ -2,15 +2,31 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(deftype Path [name])
+(deftype Path [namespace name]
+  clojure.lang.Named
+  (getName [this] (.name this))
+  (getNamespace [this] (.namespace this)))
 
-(def str->path ->Path)
+(defn path->str
+  [path]
+  (str (namespace path) "#" (name path)))
+
+(defn str->path
+  [str]
+  (let [[ns name] (str/split str #"#")]
+    (->Path ns name)))
+
+(comment
+  (name (->Path "welcome" "index"))
+  (namespace (->Path "welcome" "index"))
+  (path->str (->Path "welcome" "index"))
+  (path->str (str->path "welcome#index"))
+)
+
 (def path-reader str->path)
 
 (defn path? [p]
   (instance? Path p))
-
-(defn root? [p] (= (.name p) "/"))
 
 (defn- with-ext
   [file ext]
@@ -25,37 +41,43 @@
   (with-ext "welcome" ".html")
   )
 
+(defn path->fs-path
+  "Convert a path into a file system path"
+  ([path] (str (namespace path) "/" (name path)))
+  ([path ext] (with-ext (path->fs-path path) ext)))
+
+(defn path->symbol
+  "Convert a path into a symbol"
+  [path]
+  (let [ns (str/replace (namespace path) #"/" ".")]
+    (symbol ns (name path))))
+
+(comment
+  (path->fs-path (str->path "welcome#index"))
+  (path->fs-path (str->path "welcome#index") "html")
+  #pbnj/path "welcome#index"
+  (path->symbol #path "pbnj/paths#str->path")
+  )
+
 (defn path->file
-  ([path]
-   (io/as-file (.name path)))
-  ([parent path]
-   (io/file parent (.name path)))
+  ([path ext]
+   (io/file (path->fs-path path ext)))
   ([parent path ext]
-   (io/file parent (with-ext (.name path) ext))))
+   (io/file parent (path->fs-path path ext))))
 
 (defn exists?
-  ([path]
-   (.exists (path->file path)))
-  ([parent path]
-   (.exists (path->file parent path)))
+  ([parent ext]
+   (.exists (path->file parent ext)))
   ([parent path ext]
    (.exists (path->file parent path ext))))
 
 (defn fetch
-  ([path]
-   (when (exists? path)
-     (path->file path)))
-  ([parent path]
-   (when (exists? parent path)
-     (path->file parent path)))
+  ([path ext]
+   (when (exists? path ext)
+     (path->file path ext)))
   ([parent path ext]
    (when (exists? parent path ext)
      (path->file parent path ext))))
-
-(defn parts [path]
-  (if (root? path)
-    [""]
-    (str/split (.name path) #"/")))
 
 (defn locate
   [path & {:keys [parents formats]}]
@@ -69,11 +91,11 @@
    (filter java.io.File/.exists)))
 
 (comment
-  (fetch #pbnj/path "test/resources/welcome/index.html")
-  (fetch #pbnj/path "test/resources/welcome/index.php")
-  (fetch "test/resources" #pbnj/path "welcome/index" "html")
+  (fetch #pbnj/path "test/resources/welcome#index" "html")
+  (fetch #pbnj/path "test/resources/welcome#index" "php")
+  (fetch "test/resources" #pbnj/path "welcome#index" "html")
 
-  (locate #pbnj/path "welcome/index" :parents #{"test/resources"} :formats #{:html :php :js})
+  (locate #path "welcome#index" :parents #{"test/resources"} :formats #{:html :php :js})
 
-  (zipmap #{1} #{:a :b :c})
+  #path "welcome#index"
   )
--- test/pbnj/paths_test.clj
+++ test/pbnj/paths_test.clj
@@ -6,30 +6,26 @@
 
 (deftest locate-test
   (let [expected (io/file "test/resources/welcome/index.html")
-        actual (-> #pbnj/path "welcome/index"
+        actual (-> #path "welcome#index"
                    (path/locate :parents #{"test/resources"} :formats #{:html})
                    first)]
     (is (= expected actual))))
 
 (deftest path->file-test
   (let [expected (io/file "test/resources/welcome/index.html")]
-    (testing "just path"
-      (is (= expected (path/path->file #pbnj/path "test/resources/welcome/index.html"))))
-    (testing "parent and path"
-      (is (= expected (path/path->file "test/resources" #pbnj/path "welcome/index.html"))))
-    (testing "parent, path and extension"
-      (let [file (path/path->file "test/resources" #pbnj/path "welcome/index" "html")]
+    (testing "path and extension"
+      (is (= expected (path/path->file #path "test/resources/welcome#index" "html"))))
+    (testing "path and extension with a leading dot"
+      (let [file (path/path->file #pbnj/path "test/resources/welcome#index" ".html")]
         (is (= expected file))))
-    (testing "parent, path and extension with a leading dot"
-      (let [file (path/path->file "test/resources" #pbnj/path "welcome/index" ".html")]
+    (testing "parent, path and extension"
+      (let [file (path/path->file "test/resources" #pbnj/path "welcome#index" "html")]
         (is (= expected file))))))
 
 (deftest exists?-test
   (testing "path exists"
-    (is (path/exists? #pbnj/path "test/resources/welcome/index.html"))
-    (is (path/exists? "test/resources" #pbnj/path "welcome/index.html"))
-    (is (path/exists? "test/resources" #pbnj/path "welcome/index" "html")))
+    (is (path/exists? #path "test/resources/welcome#index" "html"))
+    (is (path/exists? "test/resources" #path "welcome#index" "html")))
   (testing "path doesn't exist"
-    (is (not (path/exists? #pbnj/path "test/resources/welcome/index.php")))
-    (is (not (path/exists? "test/resources" #pbnj/path "welcome/index.php")))
-    (is (not (path/exists? "test/resources" #pbnj/path "welcome/index" "php")))))
+    (is (not (path/exists? #path "test/resources/welcome#index" "php")))
+    (is (not (path/exists? "test/resources" #pbnj/path "welcome#index" "php")))))
