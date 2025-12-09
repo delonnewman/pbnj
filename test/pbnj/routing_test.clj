3e75311913c591cb63df24f9fea03a0118ae975b
Delon R. Newman <contact@delonnewman.name>
2025-12-08 20:18:34 -0500

    Update paths

--- test/pbnj/routing_test.clj
+++ test/pbnj/routing_test.clj
@@ -7,5 +7,5 @@
     (let [tree
           (r/route-tree
            (r/routes
-            (r/page "/" :to #pbnj/path "welcome/index" :name "root")))]
-      (is (= tree {"" {:route.tree/get "resources/welcome/index.html"}})))))
+            (r/page "/" :to #path "welcome#index" :name "root")))]
+      (is (= tree {"" {:route.tree/get #path "welcome#index"}})))))
