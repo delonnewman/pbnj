35f56782ad7fa75910c695f211213ecd5e82e72c
Delon R. Newman <contact@delonnewman.name>
2025-12-31 23:56:06 -0500

    Playing with CL

--- pbnj.lisp
+++ pbnj.lisp
@@ -1,10 +1,52 @@
-(defstruct action :namespace :name :meta)
+(defmacro comment (&rest _))
+(setf (fdefinition 'identical?) #'eq)
+(setf (fdefinition 'nil?) #'null)
+(setf (fdefinition 'string?) #'stringp)
+(setf (fdefinition 'type?) #'typep)
 
-(defvar root (make-action :namespace "welcome" :name "index" :meta '()))
-(action-name root)
-(action-namespace root)
-(action-meta root)
+(defstruct (action (:predicate action?))
+  (namespace
+   nil
+   :type string
+   :read-only t)
+  (name
+   nil
+   :type string
+   :read-only t)
+  (meta
+   nil
+   :type list
+   :read-only t))
 
-(defvar act (make-action))
-(setf (action-name act) "hey")
-act
+(defun action (namespace name &optional meta)
+  (if meta
+    (make-action :namespace namespace :name name :meta meta
+    (make-action :namespace namespace :name name)))
+
+(defun action= (one another)
+  (and
+   (string= (action-namespace one) (action-namespace one))
+   (string= (action-name one) (action-name one))))
+
+(defun action->string (action)
+  (concatenate 'string (action-namespace action) "#" (action-name action)))
+
+(comment
+ (setq root (action "welcome" "index"))
+ (action? root)
+ (type? root 'action)
+ (action-name root)
+ (action-namespace root)
+ (action-meta root)
+ (action->string root)
+
+ (equal (action "welcome" "index") (action "welcome" "index"))
+ (equalp (action "welcome" "index") (action "welcome" "index"))
+ (identical? (action "welcome" "index") (action "welcome" "index"))
+ (identical? root root)
+ (eql (action "welcome" "index") (action "welcome" "index"))
+ (action=
+  (action "welcome" "index")
+  (action "welcome" "index" '((hey "you"))))
+ (string? "Hey")
+ )
