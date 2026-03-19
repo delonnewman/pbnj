e6870b12095822fc083c1032ee9c0fd23fd04876
Delon Newman <contact@delonnewman.name>
2026-03-19 11:27:13 -0400

    Read from site.edn

--- src/clj/pbnj/site.clj
+++ src/clj/pbnj/site.clj
@@ -1,10 +1,18 @@
 (ns pbnj.site
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(def ^:dynamic *config-file* "site.edn")
 
 (defn create-basis [& {:keys [dir]}]
-  (let [f (io/file dir)]
-    (if (.exists f)
-      {:site/root-dir (.getAbsoluteFile f)}
-      (throw (ex-info "Invalid directory" { :dir dir })))))
+  (let [f (-> dir io/file .getAbsoluteFile)]
+    (if-not (.exists f)
+      (throw (ex-info "Invalid directory" { :dir dir }))
+      (let [base (-> f (io/file *config-file*) .getAbsoluteFile)]
+        (if-not (.exists base)
+          #:site{:root-dir f}
+          (merge
+           #:site{:root-dir f, :config base}
+           (edn/read-string (slurp base))))))))
 
 (defn build [site])
